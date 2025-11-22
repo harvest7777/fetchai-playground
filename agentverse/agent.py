e1128e9a244393fa16828b1e90e63f7b8fc5c6a0
harvest7777 <333harvest.moon333@gmail.com>
2025-11-21 22:35:08 -0800

    chat proto

--- agentverse/agent.py
+++ agentverse/agent.py
@@ -1,59 +1,70 @@
+"""
+This simply registers an agent to the AgentVerse by enabling chat protocol and publishing the manifest.
+Don't worry if you don't understand what this exactly means. It's essentially the mandatory functions
+your agent must implement to be compliant with the AgentVerse protocol.
+"""
+
 from datetime import datetime
 from uuid import uuid4
 import os
 from dotenv import load_dotenv
-# from openai import OpenAI
-from uagents import Context, Protocol, Agent
+from uagents import Context, Protocol, Agent, Model
+
 from uagents_core.contrib.protocols.chat import (
     ChatAcknowledgement,
     ChatMessage,
-    StartSessionContent,
-    EndSessionContent,
-    AgentContent,
     TextContent,
     chat_protocol_spec,
 )
 load_dotenv()
 
 agent = Agent(
-    name="xiaoje",
-    seed="skladfjlkdkljfweoifjweofwiofsmdflk",
-    port=8001,
+    name="heyyy aganet nent",
+    seed=os.getenv("AGENT_SEED_PHRASE"),
+    port=8000,
     mailbox=True,
 )
+
+# This is needed to be compliant with AgentVerse's chat protocol
 protocol = Protocol(spec=chat_protocol_spec)
 
-# Chat protocol so you can message through asi one and agentverse
+# This is needed to be compliant with AgentVerse's chat protocol
+# This decorator in particular makes this function get called when we get a message through 
+# chat protocol
 @protocol.on_message(ChatMessage)
 async def handle_message(ctx: Context, sender: str, msg: ChatMessage):
     await ctx.send(
         sender,
         ChatAcknowledgement(timestamp=datetime.now(), acknowledged_msg_id=msg.msg_id),
     )
+
+    # Hardcoded response, your logic from LangGraph/LangChain/CrewAi/etc would go here
     response = "meowwww"
+
+    # Here's an example on how to check various metadata such as first message or if the message
+    # was sent by a user
     # is_start_of_chat: AgentContent = isinstance(msg.content[-1], StartSessionContent)
     is_user_message = isinstance(msg.content[-1], TextContent)
+
     ctx.logger.info(msg)
+
     if is_user_message:
         await ctx.send(sender, ChatMessage(
             timestamp=datetime.now(),
             msg_id=uuid4(),
             content=[
                 TextContent(type="text", text=response),
-                # This will end the chat session
+                # This will end the chat session on each messsage (no conversation)
                 # EndSessionContent(type="end-session") 
             ]
         ))
 
+# This is needed to be compliant with AgentVerse's chat protocol
 @protocol.on_message(ChatAcknowledgement)
 async def handle_ack(ctx: Context, sender: str, msg: ChatAcknowledgement):
     pass
 
-"""
-input: u see a room and are like turn this chair into a 3d mdoel
-"""
-
-# I believe you have to have this to register it to AgentVerse
+# This is needed to get registered to the AgentVerse
 agent.include(protocol, publish_manifest=True)
 if __name__ == "__main__":
     agent.run()
